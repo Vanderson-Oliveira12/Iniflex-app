7d415a0b8dbc1b4e3b1075a4935cf6b9072f5427
Vanderson <vanderson.doliveira14@gmail.com>
2026-09-14 17:10:47 -0300

    chore: adicionado regra salario formato

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,6 +1,9 @@
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.text.NumberFormat;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.Locale;
 
 public class Funcionario extends Pessoa {
 
@@ -32,17 +35,21 @@ public class Funcionario extends Pessoa {
     @Override
     public String toString() {
         var sb = new StringBuilder();
-        var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        var dataFormatada = getDataNascimento().format(formatter);
+        var regraFormatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        var regraFormatoSalario = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+
+        var dataFormatada = getDataNascimento().format(regraFormatoData);
+        var salarioFormatado = regraFormatoSalario.format(getSalario());
 
         sb.append("---------------------------").append("\n");
         sb.append("Dados do funcionário \n");
         sb.append("Nome: ").append(getNome()).append("\n");
         sb.append("Data Nascimento: ").append(dataFormatada).append("\n");
         sb.append("Função: ").append(getFuncao()).append("\n");
-        sb.append("Salário: ").append(getSalario()).append("\n");
+        sb.append("Salário: ").append(salarioFormatado).append("\n");
         sb.append("---------------------------").append("\n");
 
         return sb.toString();
     }
+
 }
